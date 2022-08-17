2d54f1018d57d56d56d6e9c942794210d8df685e
Maciej Leszczyński <maciek15557@gmail.com>
2022-08-17 22:21:24 +0200

    add rent history

--- base/urls.py
+++ base/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name="home"),
-    path('add_book/',views.addBook, name="addBook"),
-    path('login/',views.loginPage, name="login"),
-    path('logout/',views.logoutPage, name="logout"),
-    path('register/',views.registerUser, name="register"),
-    path('searchbook/',views.searchbook, name="search"),
-    path('my_book/',views.mybook, name="my_book"),
+    path('', views.home, name="home"),
+    path('add_book/', views.addBook, name="addBook"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutPage, name="logout"),
+    path('register/', views.registerUser, name="register"),
+    path('searchbook/', views.searchbook, name="search"),
+    path('my_book/', views.mybook, name="my_book"),
     path('books/', views.books, name='books'),
-    path('rent_book/<str:pk>', views.rentBook, name = 'rentbook'),
-    path('return_book/<str:pk>', views.returnBook, name = 'return-book')  
-          
+    path('rent_book/<str:pk>', views.rentBook, name='rentbook'),
+    path('return_book/<str:pk>', views.returnBook, name='return-book'),
+    path('rent_history/', views.returnHistory, name='return-history')
+
 ]
--- base/views.py
+++ base/views.py
@@ -7,31 +7,38 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from dateutil.relativedelta import relativedelta
-from datetime import date 
+from datetime import date
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'base/home.html')
 
+
 def books(request):
     book = Book.objects.all()
-    context = {'books' : book}
+    context = {'books': book}
     return render(request, 'base/book.html', context)
+
 
 def searchbook(request):
     if request.method == 'POST':
         rentBook(request)
     if request.method == 'GET':
-        q=request.GET.get('q')
-        book = Book.objects.filter(Q(title__icontains = q) | Q(author__icontains =q))   
-        return render(request, 'base/book.html', {'books' : book})
+        q = request.GET.get('q')
+        book = Book.objects.filter(
+            Q(title__icontains=q) | Q(author__icontains=q))
+        return render(request, 'base/book.html', {'books': book})
+
 
 def mybook(request):
-    book = Book.objects.filter(rents__user = request.user.id)
-    rent = rents.objects.filter(user = request.user.id)
-   
-    context = {'books' : book, 'rents' : rent}
+    book = Book.objects.filter(rents__user=request.user.id)
+    rent = rents.objects.filter(user=request.user.id)
+
+    context = {'books': book, 'rents': rent}
     return render(request, 'base/my_book.html', context)
+
 
 def addBook(request):
     form = BookForm()
@@ -44,49 +51,55 @@ def addBook(request):
     context = {'form': form}
     return render(request, 'base/add_book.html', context)
 
+
 def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        
+
         try:
             user = User.objects.get(username=username)
         except:
             messages.error(request, 'User does not exist')
 
-        user = authenticate(request, username=username,password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-             messages.error(request, 'Username or password dont match')
+            messages.error(request, 'Username or password dont match')
 
-    context={}
+    context = {}
     return render(request, 'base/login.html', context)
+
 
 def logoutPage(request):
     logout(request)
     return redirect('home')
 
-def registerUser(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("home")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request=request, template_name="base/register.html", context={"register_form":form})
 
-def rentBook(request,pk):
-    selectBook =  Book.objects.get(id = pk)
+def registerUser(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("home")
+        messages.error(
+            request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="base/register.html", context={"register_form": form})
+
+
+def rentBook(request, pk):
+    selectBook = Book.objects.get(id=pk)
     if request.method == "POST":
         currentUser = request.user
         date_r = date.today() + relativedelta(weeks=+3)
         if selectBook.free:
-            rents.objects.create(user = currentUser, book = selectBook,return_date = date_r )
+            rents.objects.create(
+                user=currentUser, book=selectBook, return_date=date_r)
             selectBook.free = 0
             selectBook.save(update_fields=['free'])
             return redirect('my_book')
@@ -94,15 +107,21 @@ def rentBook(request,pk):
             messages.error(request, "Book is already taken")
     return render(request, 'base/add_rent.html', {'obj': selectBook})
 
-def returnBook(request,pk):
-    selectBook = Book.objects.get(id = pk)
-    selectRent = rents.objects.get(book = pk) 
+
+def returnBook(request, pk):
+    selectBook = Book.objects.get(id=pk)
+    selectRent = rents.objects.get(book=pk)
     if request.method == "POST":
         if selectBook.free == 0:
             currentUser = request.user
-            Returns.objects.create(user = currentUser, book = selectBook)
+            Returns.objects.create(user=currentUser, book=selectBook)
             selectBook.free = 1
             selectBook.save(update_fields=['free'])
             selectRent.delete()
             return redirect('my_book')
     return render(request, 'base/add_return.html', {'obj': selectBook})
+
+
+def returnHistory(request):
+    Return = Returns.objects.filter(user=request.user.id)
+    return render(request, 'base/rent_history.html', {'retruns': Return})
